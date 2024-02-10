0f20a0997222a9fe1cdc5d60e9f8987d52d3a9ec
tirlochanarora16 <tirlochan.arora16@gmail.com>
2024-02-11 00:29:03 +0530

    added mui and remaining api paths

--- frontend/src/components/api/api.ts
+++ frontend/src/components/api/api.ts
@@ -4,4 +4,8 @@ export const apiPaths = {
   getSurveys: () => "surveys",
   getSurveyQuestions: (id: string) => `questions/${id}`,
   answerQuestion: (id: string) => `questions/answer/${id}`,
+  getSurveyAnswers: (id: string) => `surveys/${id}`,
+  createSurvey: () => `/surveys/create`,
+  createSurveyQuestion: (id: string) => `/questions/create/${id}`,
+  updateSurveyQuestion: (id: string) => `/questions/${id}`,
 };
